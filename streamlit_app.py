0e55b1ab3aaee0ff21c29f389d3cc88a498ccf95
sfjohnb <john@johnbarnesfamily.com>
2025-03-11 19:51:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.text(smoothiefroot_response.json())
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
     #st.write(ingredients_string)
 
@@ -54,6 +57,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!',icon='✅')
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
